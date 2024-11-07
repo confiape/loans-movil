b229974636b8131ad989196785e6bbe1bc077506
warre aroni <warren.aroni@provistacorp.com>
2024-11-07 11:32:13 -0500

    Add filter on sort tables

--- app/src/main/java/org/confiape/loan/core/Constants.kt
+++ app/src/main/java/org/confiape/loan/core/Constants.kt
@@ -1,9 +1,8 @@
 package org.confiape.loan.core
 
 object AppConstants {
-//    const val BaseUrl: String = "https://www.confiape.org"
-//    const val BaseUrl: String = "https://dev-01.confiape.org"
-        const val BaseUrl: String = "https://c096-209-45-79-70.ngrok-free.app"
+    const val BaseUrl: String = "https://dev-01.confiape.org"
+//        const val BaseUrl: String = "https://c096-209-45-79-70.ngrok-free.app"
     const val AuthenticationToken: String = "AuthenticationToken"
     const val AuthorizationToken: String = "AuthorizationToken"
     const val Tag = "AppLoanLog";
